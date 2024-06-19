71bb18e6db5fb2f7cd63b0b18198f383c7ec74ea
12bia <129787681+12bia@users.noreply.github.com>
2024-06-18 21:13:45 -0300

    Finalizado o TIC TAC TOE MOBILE
    Foi finalizado o aplicativo TIC TAC TOE, ou jogo da velha, usando kivy

---
+++ home.py
@@ -0,0 +1,59 @@
+from kivy.app import App
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.button import Button
+from kivy.uix.image import Image
+from kivy.uix.label import Label
+from kivy.uix.screenmanager import Screen, ScreenManager
+
+
+class Home(Screen):#FloatLayout
+    def __init__(self, **kwargs):
+        super(Screen,self).__init__(**kwargs)
+        
+        layout = FloatLayout()
+
+        # Adicionar a imagem de fundo ao layout,  allow_stretch=True, keep_ratio=False
+        image_pag_home = Image(source='img_pagHome_label.png', allow_stretch=True, keep_ratio=False)
+
+        # titulo_game = Label(text='Tic tac Toe', font_size=24)
+
+        # Adicionar o Label ao layout
+        # layout.add_widget(titulo_game)
+
+        
+
+        # Adicionar botão de imagem sobre a imagem de fundo
+        button_play = Button(background_normal='img_btnPlay_home.png',
+                             background_down='img_btnPlay_home.png',
+                             size_hint=(None,None),
+                             pos_hint={'center_x': 0.5, 'center_y': 0.5},# o size_hint serve para mecher na largura do botão
+                             size=(600, 100))
+        button_play.bind(on_press=self.on_button_press_play)  # Adiciona uma função de callback ao pressionar o botão
+
+        button_exit = Button(background_normal='img_btnExit_home.png',
+                             background_down='img_btnExit_home.png',
+                             size_hint=(None,None),
+                             pos_hint={'center_x': 0.5, 'center_y': 0.2},
+                             size=(600, 100))
+        button_exit.bind(on_press=self.on_button_press_exit)  # Adiciona uma função de callback ao pressionar o botão
+        
+        layout.add_widget(image_pag_home)
+        layout.add_widget(button_play)
+        layout.add_widget(button_exit)
+        self.add_widget(layout)
+
+    def on_button_press_play(self, instance):
+        self.manager.current = "menu" 
+        
+        
+        
+    # DEF SAIR
+    def on_button_press_exit(self, instance):
+        # Função para encerrar o aplicativo
+        App.get_running_app().stop()
+  
+
+    
+        
+
+
---
+++ main.py
@@ -0,0 +1,27 @@
+from home import Home
+from game import Game
+from menu import Menu
+from kivy.uix.floatlayout import FloatLayout
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+
+
+class TicTacToeApp(App):
+    def build(self):
+        # Window.size = (430, 932)
+
+        tela = ScreenManager()
+        tela.add_widget(Home(name = "home")) # para adicionar outro tem que: Colocar o nome da class(name = "Nome da classe tbm")
+        tela.add_widget(Game(name = "game"))
+        tela.add_widget(Menu(name = "menu")) # para adicionar outro tem que: Colocar o nome da class(name = "Nome da classe tbm")
+        tela.current = "menu"
+        tela.current = "home"
+        
+       
+        return tela
+    
+
+    
+if __name__ == "__main__":
+    TicTacToeApp().run()
